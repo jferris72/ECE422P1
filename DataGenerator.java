be5f8b5fd1039dd3bbda5980c8b1ab972a42adb6
jferris72 <jferris@ualberta.ca>
2017-03-04 12:08:03 -0700

    remade working data generator

--- DataGenerator.java
+++ DataGenerator.java
@@ -1,21 +1,25 @@
+import java.util.Random;
+import java.io.PrintWriter;
+
+
 public class DataGenerator {
 	public static void main (String[] args) {
 		String fileName = args[0];
-		Int numberValues = Integer.parseInt(args[1]);
+		int numberValues = Integer.parseInt(args[1]);
 		writeFile(fileName, numberValues);
 
 	}
 
-	public void writeFile(String fileName, Int numberValues) {
+	public static void writeFile(String fileName, int numberValues) {
 		try {
 			Random randomGenerator = new Random();
 			PrintWriter writer = new PrintWriter(fileName, "UTF-8");
 			for (int i = 0; i < numberValues; i++) {
 				int random = randomGenerator.nextInt();
-				writer.println(String.parseInt(random));
+				writer.println(Integer.toString(random));
 			}
 			writer.close();
-		} catch (IOException e) {
+		} catch (Exception e) {
 			System.out.println("Failed to write to file");
 		}
 	}
